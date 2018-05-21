b0d1cde65dc907b9c0223b6e1d258361e841ada0
Kemal Yenilmez <kemalyenilmez@gozen.systems>
2018-05-21 14:03:33 +0300

    Travis support

--- __test__/actions/index.test.js
+++ __test__/actions/index.test.js
@@ -3,8 +3,7 @@ import * as actions from '../../src/lib/actions';
 import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import fetchMock from 'fetch-mock'
-import expect from 'expect'
-
+ 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
@@ -18,7 +17,7 @@ describe('Fetch Quote Pair', () => {
 
     const data = [{"symbol":"EURUSD","bid":1.17936,"ask":1.17936,"price":1.17936,"timestamp":1526565096},{"symbol":"GBPJPY","bid":149.359,"ask":149.364,"price":149.3615,"timestamp":1526565096},{"symbol":"AUDUSD","bid":0.75031,"ask":0.75031,"price":0.75031,"timestamp":1526565096}];
     const symbols = "EURUSD,GBPJPY,AUDUSD"
-    const api_key = "TEST_API_KEY"
+    const api_key = "VqiSQ7khg3SbYT37dHYogD7KqEckxoz8"
     fetchMock
       .getOnce(`mock_url`, { body: data, headers: { 'content-type': 'application/json' } })
 
@@ -30,7 +29,46 @@ describe('Fetch Quote Pair', () => {
 
     return store.dispatch(actions.getQuotes(symbols, api_key))
     .then(() => {
-      expect(store.getActions()).toEqual(expectedActions)
+
+      expect.extend({
+        toContainRequest(received, argument) {
+          const type = received[0].type;
+          const pass = (type === constants.FETCH_QUOTES_REQUEST) ? true :  false
+
+          if (pass) {
+            return {
+              pass: true,
+              message: () => `expected FETCH_QUOTES_REQUEST `,
+            }
+          } else {
+            return {
+              pass: false,
+              message: () => `expected but received ${type}`,
+            }
+          }
+        }
+      })
+
+      expect.extend({
+        toContainQuotes(received, argument) {
+          const type = received[1].type;
+          const pass = (type === constants.GET_QUOTES) ? true :  false
+
+          if (pass) {
+            return {
+              pass: true,
+              message: () => `expected GET_QUOTES `,
+            }
+          } else {
+            return {
+              pass: false,
+              message: () => `expected but received ${type}`,
+            }
+          }
+        }
+      })      
+      expect(store.getActions()).toContainRequest(expectedActions)
+      expect(store.getActions()).toContainQuotes(expectedActions)
     })
   })
 })
